4b8bd8ee1b36068aa021ed8c047a8a33561d1628
jamieilagan <ilaganjanmaridel@gmail.com>
2025-11-24 20:13:32 +0800

    organized files

---
+++ assets/projectsWD.js
@@ -0,0 +1,20 @@
+var projects = [
+    {
+        image: "assets/images/activity4.jpg",
+        Activity: "Activity #4",
+        description: "<strong>Samsung Galaxy S24 Ultra</strong> - a hardware device chosen for showcase.",
+        link: "https://jamieilagan.github.io/first-project/"
+    },
+    {
+        image: "assets/images/activity5.jpg",
+        Activity: "Activity #5",
+        description: "A showcase for the Perfect Cone of the Philippines<strong> - Mayon Volcano.</strong>",
+        link: "https://jamieilagan.github.io/second-project/"
+    },
+    {
+        image: "assets/images/activity6.jpg",
+        Activity: "Activity #6",
+        description: "A website for <strong>Eskinita Art Farm</strong> with a JavaScript-powered art gallery.",
+        link: "https://jamieilagan.github.io/third-project/"
+    }
+];
